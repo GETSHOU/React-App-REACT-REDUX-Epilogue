1a42a3ad6d0a73c09faeeffa8e648b3ada07eb51
Borisov Sergei <BorisovSF.mailbox@gmail.com>
2023-08-19 23:19:06 +0300

    renamed reducer file

--- src/ReactApp/store/index.js
+++ src/ReactApp/store/index.js
@@ -1,4 +1,4 @@
 import { legacy_createStore as createStore } from 'redux';
-import fieldReducer from './reducers/index';
+import fieldReducer from './reducers/field-reducer';
 
 export const store = createStore(fieldReducer);
